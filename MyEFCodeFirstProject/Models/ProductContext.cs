0df18fcbbacf97332bcd5e63dfd0ceb2fd9bce1f
Chien Vu <chien.vh@gmail.com>
2016-10-28 11:31:50 +0700

    Initial commit
    Signed-off-by: Chien Vu <chien.vh@gmail.com>

---
+++ MyEFCodeFirstProject/Models/ProductContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyEFCodeFirstProject.Models
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext()
+            : base("name=ProductContext")
+        { }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Defines Id is primary key and auto increment.
+            modelBuilder.Entity<Product>().HasKey(x => x.Id);
+            modelBuilder.Entity<Product>().Property(x => x.Id)
+                                          .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
